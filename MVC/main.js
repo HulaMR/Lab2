ac4a5b8e1489ebbeae7dc7a9c6f4cba2b9014912
HulaMR <qwertymishagula13@gmail.com>
2022-05-26 20:13:54 +0300

    1

--- MVC/main.js
+++ MVC/main.js
@@ -1,124 +1,115 @@
-import controller from './Controller.js';
+model = { 
+	items: [],
 
+	add: function(item){
+		list_item = { text: item, completed: false }
+		this.items.push(list_item)
+	},
 
+	complete: function(item_index){
+		this.items[item_index].completed = !this.items[item_index].completed
+	},
 
-function start (e){
-	controller.addItem(e)
+	del: function (item_index){
+		this.items.splice(item_index, 1)
+	}
 
 }
 
-// model = { 
-// 	items: [],
+view = {
 
-// 	add: function(item){
-// 		list_item = { text: item, completed: false }
-// 		this.items.push(list_item)
-// 	},
-
-// 	complete: function(item_index){
-// 		this.items[item_index].completed = !this.items[item_index].completed
-// 	},
-
-// 	del: function (item_index){
-// 		this.items.splice(item_index, 1)
-// 	}
-
-// }
-
-// view = {
-
-// 	render: function() {
+	render: function() {
 		
-// 		if (model.items.length != 0) 
-// 		{
+		if (model.items.length != 0) 
+		{
 
-// 			list = document.getElementById("list")
+			list = document.getElementById("list")
 
-// 			for (var i = model.items.length - 1; i >= 0; i--) 
-// 			{
+			for (var i = model.items.length - 1; i >= 0; i--) 
+			{
 
-// 				item = document.createElement('li');
-// 				span = document.createElement('span');
-// 				check = document.createElement('a');
-// 				cross = document.createElement('a');
-// 				iconCheck = document.createElement('c')
-// 				iconCross = document.createElement('t');
+				item = document.createElement('li');
+				span = document.createElement('span');
+				check = document.createElement('a');
+				cross = document.createElement('a');
+				iconCheck = document.createElement('c')
+				iconCross = document.createElement('t');
 				
-// 				item.className = "item"
-// 				span.className = "item-text"
-// 				check.className = "item-complete"
-// 				cross.className = "item-delete"
+				item.className = "item"
+				span.className = "item-text"
+				check.className = "item-complete"
+				cross.className = "item-delete"
 				
-// 				span.textContent = model.items[i].text
+				span.textContent = model.items[i].text
 				
 				
-// 				if (model.items[i].completed) 
-// 				{
-// 					span.setAttribute("style", "text-decoration: line-through; color: rgb(27, 146, 37)")
-// 				}
+				if (model.items[i].completed) 
+				{
+					span.setAttribute("style", "text-decoration: line-through; color: rgb(27, 146, 37)")
+				}
 
 
-// 				iconCheck.setAttribute("class", "bi bi-bookmark-check")
-// 				iconCheck.setAttribute("data-id", i)
-// 				iconCross.setAttribute("class", "bi bi-trash")
-// 				iconCross.setAttribute("data-id", i)
-
-				
-// 				check.setAttribute("onclick", "controller.completeItem('" + i + "')")
-// 				cross.setAttribute("onclick", "controller.deleteItem('" + i + "')")
+				iconCheck.setAttribute("class", "bi bi-bookmark-check")
+				iconCheck.setAttribute("data-id", i)
+				iconCross.setAttribute("class", "bi bi-trash")
+				iconCross.setAttribute("data-id", i)
 
 				
-// 				check.appendChild(iconCheck)
-// 				cross.appendChild(iconCross)
-// 				item.appendChild(span)
-// 				item.appendChild(check)
-// 				item.appendChild(cross)
-// 				list.appendChild(item);
-// 			}		
-// 		} 
-// 	}
-// }
+				check.setAttribute("onclick", "controller.completeItem('" + i + "')")
+				cross.setAttribute("onclick", "controller.deleteItem('" + i + "')")
 
-// controller = {
+				
+				check.appendChild(iconCheck)
+				cross.appendChild(iconCross)
+				item.appendChild(span)
+				item.appendChild(check)
+				item.appendChild(cross)
+				list.appendChild(item);
+			}		
+		} 
+	}
+}
 
-// 	init: function() {
-// 		this.clearList();	
-// 		view.render()
-// 	},
+controller = {
+
+	init: function() {
+		this.clearList();	
+		view.render()
+	},
 	
 
-// 	addItem: function(e) {
-// 		if ((e.code == "Enter") || (e.code == "NumpadEnter")) {
-// 			if (((document.getElementById("add-item").value != "") && (document.getElementById("add-item").value != " "))) 
-// 			{
-// 				item = document.getElementById("add-item").value;
-// 	        	model.add(item)
+	addItem: function(e) {
+		if ((e.code == "Enter") || (e.code == "NumpadEnter")) {
+			if (((document.getElementById("add-item").value != "") && (document.getElementById("add-item").value != " "))) 
+			{
+				item = document.getElementById("add-item").value;
+	        	model.add(item)
 
-// 				document.getElementById("add-item").value = ""
-// 				this.clearList();	
-// 				view.render()	
-// 			}
-// 	    } 
-// 	},
+				document.getElementById("add-item").value = ""
+				this.clearList();	
+				view.render()	
+			}
+	    } 
+	},
 
 	
-// 	completeItem: function(item_index) {
+	completeItem: function(item_index) {
 
-// 		model.complete(item_index)
-// 		this.clearList();	
-// 		view.render()
-// 	},
+		model.complete(item_index)
+		this.clearList();	
+		view.render()
+	},
 
-// 	clearList: function() {
-// 		var range = document.createRange();
-// 		range.selectNodeContents(document.getElementById("list"));
-// 		range.deleteContents();
-// 	},
+	clearList: function() {
+		var range = document.createRange();
+		range.selectNodeContents(document.getElementById("list"));
+		range.deleteContents();
+	},
 
-// 	deleteItem: function(item_index) {
+	deleteItem: function(item_index) {
 
-// 		model.del(item_index)
-// 		this.clearList();	
-// 		view.render()
-// 	}
-// }
+		model.del(item_index)
+		this.clearList();	
+		view.render()
+	}
+}
